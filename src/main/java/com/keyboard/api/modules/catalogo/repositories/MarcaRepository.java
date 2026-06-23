2ae5d7ea004642ecec7142d69fce6a4cee899088
Drayer <drayeryoncley11@gmail.com>
2026-06-23 10:07:17 -0400

    Cambios

--- src/main/java/com/keyboard/api/modules/catalogo/repositories/MarcaRepository.java
+++ src/main/java/com/keyboard/api/modules/catalogo/repositories/MarcaRepository.java
@@ -1,4 +1,19 @@
-package com.tecladosstore.tienda.repository;
+package com.keyboard.api.modules.catalogo.repositories;
 
 import com.tecladostore.tienda.model.Marca;
-import org
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+
+@Repository
+public interface MarcaRepository extends JpaRepository <Marca, Long>
+{
+    //Springboot lee e nombre del metodo y genera un sql de manera automatica
+    //SELET * FROM tienda.marcas WHERE slug = ?;
+    //para prevenir SQL Injection
+    Optinal<Marca> findBySlug(String slug);
+
+    //Validar antes de insertar
+    boolean existByNombre(String nombre);
+}
