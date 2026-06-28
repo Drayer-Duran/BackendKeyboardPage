c73040d234d1d04133d5b4d959c1bae602487f67
Drayer <drayeryoncley11@gmail.com>
2026-06-28 13:29:18 -0400

    Validaciones completas

---
+++ src/main/java/com/keyboard/api/modules/shared/utils/RutValidator.java
@@ -0,0 +1,26 @@
+package com.keyboard.api.modules.shared.validations;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import com.keyboard.api.modules.shared.utils.ValidatorRut;
+
+public class RutValidator implements ConstraintValidator<RutValido, String> 
+{
+    @Override
+    public void initialize(RutValido constraintAnnotation)
+    {
+
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context)
+    {
+        if(value == null || value.trim().isEmpty())
+        {
+            return false;
+        }
+
+
+        return ValidadorRut.validarRut(value);
+    }
+}
