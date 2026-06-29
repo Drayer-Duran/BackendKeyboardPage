29b446ad1e8fd342d550b488280ba30be8cc4106
Drayer <drayeryoncley11@gmail.com>
2026-06-28 20:45:54 -0400

    se va a caer todo chavito

--- src/main/java/com/keyboard/api/modules/catalogo/dtos/MarcaRequestDTO.java
+++ src/main/java/com/keyboard/api/modules/catalogo/dtos/MarcaRequestDTO.java
@@ -1,54 +1,29 @@
 package com.keyboard.api.modules.catalogo.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-public class MarcaRequestDTO 
-{
-    //Notblamc es para verificar que el nombre recibido no sea nulo o vacio
+public class MarcaRequestDTO {
+
     @NotBlank(message = "El nombre de la marca es obligatorio")
-    @Size(max = 100, message ="El nombre no puede superar los 100 caracteres")
+    @Size(max = 100, message = "El nombre no puede superar los 100 caracteres")
     private String nombre;
-
-
 
     @NotBlank(message = "El slug es obligatorio")
     @Size(max = 120, message = "El slug no puede superar los 120 caracteres")
     private String slug;
 
-
-    //En caso de la marca tenga o no logo
     private String logoUrl;
 
+    // ==========================================
+    // GETTERS Y SETTERS
+    // ==========================================
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
 
-    
-    public String getNombre() {
-        return nombre;
-    }
+    public String getSlug() { return slug; }
+    public void setSlug(String slug) { this.slug = slug; }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getSlug() {
-        return slug;
-    }
-
-    public void setSlug(String slug) {
-        this.slug = slug;
-    }
-
-    public String getLogoUrl() {
-        return logoUrl;
-    }
-
-    public void setLogoUrl(String logoUrl) {
-        this.logoUrl = logoUrl;
-    }
-
-
-
-    @PostMapping("/api/marcas")
-    public void crearMarca(@Valid RequestBody MarcaRequestDTO datosQueLlegan){
-        //
-    }
-
+    public String getLogoUrl() { return logoUrl; }
+    public void setLogoUrl(String logoUrl) { this.logoUrl = logoUrl; }
 }
